9b8bd7c86b9d4de594b1e61dab08e042f728448c
Asmenys <76791232+Asmenys@users.noreply.github.com>
2021-12-01 23:35:43 +0000

    a bunch of functions for the game board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-
 require_relative 'display'
+
 
 class Board
   include Display
--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,43 @@
 require_relative 'board.rb'
+require_relative 'player.rb'
 
 class Game
-    def initialize
-        @board = initialize_board
-        p 'Input the name of player 1'
-        @player_one = Player.new(gets.chomp, "\u26AB")
-        p 'Input the name of player 2'
-        @player_two = Player.new(gets.chomp, "\u26AA")
+
+    def initialize(player_one_name, player_two_name)
+        @board = Board.new
+
+        @player_one = Player.new(player_one_name, "\u26AB")
+        @player_two = Player.new(player_two_name, "\u26AA")
+        @turn = 1
+        @max_turns = 42
     end
 
-    def initialize_board
-        Board.new
+    def print_board
+        @board.print_board
+    end
+
+    def get_player_input(message)
+        p message
+        input = gets.chomp
+        if validate_input(input) == false
+           input = get_player_input("Please enter a single integer as your input")
+        end
+        input.to_i
+    end
+
+    def validate_input(input)
+        result = false
+        if input.length == 1
+            if /[1-9]/.match?(input)
+                result = true
+            end
+        end
+        result
+    end
+
+    def get_name
+        row = get_player_input("Choose a row of the board")
+        column = get_player_input("Choose a column of the board")
+        [row, column]
     end
 end
